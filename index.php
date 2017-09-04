8fab4f123419d439e06afcd1a462e88993013f39
y2k6678 <sand_6678@hotmail.com>
2017-09-03 21:46:07 -0700

    Update index.php

--- index.php
+++ index.php
@@ -4,36 +4,6 @@ $url = "www."
 $json = file_get_contents($url);
 
 
-$jsonInput = '"id":"rlZiIaC",
-   "title":null,
-   "description":null,
-   "datetime":1497624461,
-   "type":"image\/jpeg",
-   "animated":false,
-   "width":370,
-   "height":320,
-   "size":57357,
-   "views":0,
-   "bandwidth":0,
-   "vote":null,
-   "favorite":false,
-   "nsfw":null,
-   "section":null,
-   "account_url":null,
-   "account_id":0,
-   "is_ad":false,
-   "in_most_viral":false,
-   "tags":[   ],
-   "ad_type":0,
-   "ad_url":"",
-   "in_gallery":false,
-   "deletehash":"kNKhDYuj5OrECCS",
-   "name":"",
-   "link":"http:\/\/i.imgur.com\/rlZiIaC.jpg"
-},
-"success":true,
-"status":200
-}'; 
 $data = json_decode($jsonInput,true);
 
 foreach($data as $inv){
