e89edd664c0869defa3c43aac399812f4af8ae0a
y2k6678 <sand_6678@hotmail.com>
2017-09-06 04:53:13 -0700

    Update index.php

--- index.php
+++ index.php
@@ -1,23 +1,4 @@
 <?php
 echo "hello"
-# include 'databaseconnect.php';
-$url = "www."
-$json = file_get_contents($url);
 
-
-$data = json_decode($jsonInput,true);
-
-foreach($data as $inv){
-    $id = $inv['id'];
-    $datetime = $inv['datetime'];
-    $type = $inv['type'];
-    $width = $inv['width'];
-    $height = $inv['height'];
-    $size = $inv['size'];
-    $link = $inv['link'];
-       $sql = "INSERT INTO mydba (id, datetime, type, width, height, size, link) VALUES ('$id', '$datetime', '$type', '$width' '$height' '$size' '$link')";
-
-    echo $sql."\n";
-
-}
 ?>
