e619965bf0986732e949d630aab96fa492caa62a
y2k6678 <sand_6678@hotmail.com>
2017-09-15 21:03:59 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,18 @@
 <html>
-  <B>ssss</B>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+<script src="http://code.jquery.com/jquery-latest.min.js"></script>
+  
+</head>
 <?php 
+header("Content-Type: application/json; charset=UTF-8");
+$obj = json_decode($_POST["x"], false);
 
+$conn = new mysqli("myServer", "myUser", "myPassword", "Northwind");
+$result = $conn->query("SELECT name FROM ".$obj->table." LIMIT ".$obj->limit);
+$outp = array();
+$outp = $result->fetch_all(MYSQLI_ASSOC);
+
+echo json_encode($outp);
 ?>
 </html>
